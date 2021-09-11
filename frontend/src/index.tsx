a5ea17fe05bd532303b2d273037312eb1e0f2ff2
Lucas Ramos <lucas2adriano@gmail.com>
2021-09-10 23:58:28 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'C:/Users/User/Documents/ws-devsuperior/projeto-sds3/frontend/src/assets/css/styles.css';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
