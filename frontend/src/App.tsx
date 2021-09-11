fd1647ba467d18f5bd095edb1aeb620aadf195dd
Lucas Ramos <lucas2adriano@gmail.com>
2021-09-11 01:21:12 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import DonutChart from "C:/Users/User/Documents/ws-devsuperior/projeto-sds3/frontend/src/components/DonutChart";
+import BarChart from "C:/Users/User/Documents/ws-devsuperior/projeto-sds3/frontend/src/components/BarChart";
 import DataTable from "C:/Users/User/Documents/ws-devsuperior/projeto-sds3/frontend/src/components/DataTable";
 import Footer from "C:/Users/User/Documents/ws-devsuperior/projeto-sds3/frontend/src/components/Footer";
 import NavBar from "C:/Users/User/Documents/ws-devsuperior/projeto-sds3/frontend/src/components/NavBar";
@@ -7,11 +9,27 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <DataTable/>
+        <div className="row px-3">s
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de vendas (%)</h5>
+            <BarChart />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de vendas (%)</h5>
+          <DonutChart />
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+        <div>
+        </div>
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
