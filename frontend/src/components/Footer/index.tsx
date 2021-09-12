7e062aad6708ee9890eedd0460f7761158c7ab3a
Lucas Ramos <lucas2adriano@gmail.com>
2021-09-12 16:52:28 -0300

    fix 2

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ function Footer() {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/devlucasramos/" target="_blank" rel="noreferrer">Lucas Ramos</a></p>
                 <p className="text-light">e esse é o meu <a href="https://github.com/devLucasRamos" target="_blank" rel="noreferrer">Github</a></p>
-            </div>
+</div>
         </footer>
     );
 }
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
       </div>
       <Footer />
     </>
-  )
+    )
 }
 
 export default Home;
